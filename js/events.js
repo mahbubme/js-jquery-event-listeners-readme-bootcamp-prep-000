674a5dca26ad166c365531e4f44c7862926362f5
Mahbubur Rahman <mail@mahbub.me>
2018-12-21 11:18:01 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -13,12 +13,14 @@ function frameIt() {
 
 function pressIt() {
   $('input').on('keydown', function(e){
-    alert("");
+    if( e.key = '71' ){
+      alert("G was pressed!");
+    }
   })
 }
   
 $(document).ready(function(){
   getIt(); 
   frameIt();
-  
+  pressIt();
 });
